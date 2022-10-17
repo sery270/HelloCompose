0d5b3a12f6dea2b08835cd412b8a5a6159a82ea3
sery270 <sery270@naver.com>
2022-10-17 17:28:50 +0900

    feat: spring 애니메이션 적용

--- BasicsCodelab/app/src/main/java/com/sery270/basicscodelab/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/sery270/basicscodelab/MainActivity.kt
@@ -3,11 +3,20 @@ package com.sery270.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.material.ButtonDefaults.outlinedButtonColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +24,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sery270.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -46,7 +59,7 @@ private fun MyApp() {
 
 @Composable
 private fun Greetings(names: List<String> = List(1000) { "$it" }) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+    LazyColumn(modifier = Modifier.padding(vertical = 8.dp)) {
         items(items = names) { name ->
             Greeting(name = name)
         }
@@ -55,6 +68,16 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 
 @Composable
 private fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.secondary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
 
     // 1. 각 아이템 (greeting) 마다 확장 여부 정보가 필요하다. 논리적으로 expanded 정보는 각 아이템이 가지고 있어야한다.
     // 2. Boolean 같은 자료형으로 상태를 저장하면 안된다.
@@ -82,26 +105,82 @@ private fun Greeting(name: String) {
 
     // MaterialTheme : 구글의 디자인 시스템
     // 따라서 배경 색(Surface의 color)만 지정해줬을 뿐이지만 텍스트의 색상도 같이 변경되는 것을 확인할 수 있음
-    Surface(
-        color = MaterialTheme.colors.secondary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+//    Surface(
+//        color = MaterialTheme.colors.background,
+//        modifier = Modifier
+//            .padding(vertical = 8.dp, horizontal = 16.dp)
+//            .border(1.dp, MaterialTheme.colors.secondary)
+//    ) {
+//        Row(modifier = Modifier.padding(24.dp)) {
+//            Column(
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(bottom = extraPadding)
+//            ) {
+//                Text(
+//                    text = "Welcome :)",
+//                    color = MaterialTheme.colors.secondary,
+//                    style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Light)
+//                )
+//                Text(
+//                    text = name, style = MaterialTheme.typography.h4.copy(
+//                        fontWeight = FontWeight.Bold
+//                    )
+//                )
+//            }
+//            // Modifier : 정렬, 애니메이션 처리, 배치, 클릭 가능 여부 또는 스크롤 가능 여부 지정, 변환을 할 수 있음
+//            // https://developer.android.com/jetpack/compose/modifiers-list?hl=ko
+//            OutlinedButton(
+//                onClick = { expanded = !expanded },
+//                colors = outlinedButtonColors(MaterialTheme.colors.secondary, Color.White)
+//            ) {
+//                Text(if (expanded) "less" else "more", color = Color.White)
+//            }
+//        }
+//    }
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Welcome :)")
-                Text(text = name)
-            }
-            // Modifier : 정렬, 애니메이션 처리, 배치, 클릭 가능 여부 또는 스크롤 가능 여부 지정, 변환을 할 수 있음
-            // https://developer.android.com/jetpack/compose/modifiers-list?hl=ko
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                Text(if (expanded) "less" else "more")
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
         }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+
     }
+
 }
 
 @Composable
@@ -114,9 +193,8 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
         ) {
             Text("Welcome to the Basics Codelab!")
             Button(
-                modifier = Modifier
-                    .padding(vertical = 24.dp),
-                onClick = onContinueClicked
+                modifier = Modifier.padding(vertical = 24.dp), onClick = onContinueClicked,
+                colors = buttonColors(MaterialTheme.colors.secondary, Color.White)
             ) {
                 Text("Continue")
             }
